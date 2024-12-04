843f1f264941f77f63585e29220625683d85c482
narracs <narracs@protonmail.ch>
2024-12-04 15:44:33 +1000

    commented out test code

--- javascript/compsci/hashmap.js
+++ javascript/compsci/hashmap.js
@@ -106,22 +106,22 @@ class HashMap {
     }
   }
 }
-const test = new HashMap();
-test.set("apple", "red");
-test.set("apple", "green");
-test.set("banana", "yellow");
-test.set("carrot", "orange");
-test.set("dog", "brown");
-test.set("elephant", "gray");
-test.set("frog", "green");
-test.set("grape", "purple");
-test.set("hat", "black");
-test.set("ice cream", "white");
-test.set("jacket", "blue");
-test.set("kite", "pink");
-test.set("lion", "golden");
-test.set("flamingo", "pink");
-test.print();
-console.log(`Get hat colour: ${test.get("hat")}`);
-console.log(`Has snake: ${test.has("snake")}`);
-console.log(`Has flamingo: ${test.has("flamingo")}`);
+// const test = new HashMap();
+// test.set("apple", "red");
+// test.set("apple", "green");
+// test.set("banana", "yellow");
+// test.set("carrot", "orange");
+// test.set("dog", "brown");
+// test.set("elephant", "gray");
+// test.set("frog", "green");
+// test.set("grape", "purple");
+// test.set("hat", "black");
+// test.set("ice cream", "white");
+// test.set("jacket", "blue");
+// test.set("kite", "pink");
+// test.set("lion", "golden");
+// test.set("flamingo", "pink");
+// test.print();
+// console.log(`Get hat colour: ${test.get("hat")}`);
+// console.log(`Has snake: ${test.has("snake")}`);
+// console.log(`Has flamingo: ${test.has("flamingo")}`);
